d0382ff60d467546baf794373b878cda85689372
Namira Pramadhani <namnam6617@gmail.com>
2025-06-06 22:57:40 +0700

    menambahkan kode untuk login state

---
+++ lib/presentation/auth/bloc/login/login_state.dart
@@ -0,0 +1,17 @@
+import 'package:canary_project/data/model/response/auth_response_model.dart';
+
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState{}
+final class LoginLoading extends LoginState{}
+final class LoginSuccess extends LoginState{
+  final AuthResponseModel responseModel;
+
+  LoginSuccess({required this.responseModel});
+}
+
+final class LoginFailure extends LoginState{
+  final String error;
+
+  LoginFailure({required this.error});
+}
