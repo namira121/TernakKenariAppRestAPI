0178af28ebb6ebb93ca7caee4612de918a812e10
Namira Pramadhani <namnam6617@gmail.com>
2025-06-06 23:29:15 +0700

    menambahkan kode register bloc

---
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:canary_project/data/repository/auth_repository.dart';
+import 'package:canary_project/presentation/auth/bloc/register/register_event.dart';
+import 'package:canary_project/presentation/auth/bloc/register/register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository authRepository;
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()){
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  )async{
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r)=> emit(RegisterSuccess(message: r)),
+    );
+  }
+}
