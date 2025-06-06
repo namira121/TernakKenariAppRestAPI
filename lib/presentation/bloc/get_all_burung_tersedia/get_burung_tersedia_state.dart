b06f8ef4abb94cb935ac36502e92e5564d8aa509
Namira Pramadhani <namnam6617@gmail.com>
2025-06-07 06:21:46 +0700

    menambahkan kode get burung tersedia state

---
+++ lib/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_state.dart
@@ -0,0 +1,17 @@
+import 'package:canary_project/data/model/response/burung_semua_tersedia_model.dart';
+
+sealed class GetBurungTersediaState {}
+
+final class GetBurungTersediaInitial extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoading extends GetBurungTersediaState {}
+
+final class GetBurungTersediaLoaded extends GetBurungTersediaState {
+  final BurungSemuaTersediaModel burungTersedia;
+  GetBurungTersediaLoaded({required this.burungTersedia});
+}
+
+final class GetBurungTersediaError extends GetBurungTersediaState {
+  final String message;
+  GetBurungTersediaError({required this.message});
+}
